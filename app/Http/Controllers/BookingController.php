9a124412ceff1f54bffb486b65345db3977bb5ed
aisenamarlina <2307017@itg.ac.id>
2026-02-05 16:12:20 +0700

    Update BookingController

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -7,9 +7,7 @@ use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
-    /**
-     * Menampilkan daftar peminjaman (Index).
-     */
+    
     public function index(Request $request)
     {
         $query = Booking::query();
